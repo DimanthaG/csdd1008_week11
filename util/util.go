e6827a13b9d60b36f773516450609bab21a07653
DimanthaG <theenslaveyer@gmail.com>
2024-11-20 16:13:26 -0500

    GU-3 | Added Documentation

--- util/util.go
+++ util/util.go
@@ -11,3 +11,29 @@ func IsPrime(n int) bool {
 	}
 	return true
 }
+
+func ReverseString(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+func SumOfSlice(slice []int) int {
+	sum := 0
+	for _, v := range slice {
+		sum += v
+	}
+	return sum
+}
+
+func Factorial(n int) int {
+	if n < 0 {
+		return -1
+	}
+	if n == 0 || n == 1 {
+		return 1
+	}
+	return n * Factorial(n-1)
+}
